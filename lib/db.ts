19622e7c39e08a45bc8e23e547b4a7cdb7dc2373
Venclise <zunnurainzahoor955@gmail.com>
2026-04-02 03:25:53 +0500

    Issue solved

--- lib/db.ts
+++ lib/db.ts
@@ -1,26 +1,37 @@
 import mongoose from "mongoose"
 
-const MONGODB_URI = process.env.MONGODB_URI!
 
-if(!MONGODB_URI) {
-    throw new Error("Mongo DB URL is not defined")
-}
+const MONGODB_URI = process.env.MONGODB_URI
 
 let cached = (global as any).mongoose
 if(!cached) {
-    cached = (global as any).mongoose = {conn: null,promise: null}
+    cached = (global as any).mongoose = {conn: null, promise: null}
 }
 
 export async function connectDB() {
-    if (cached.conn) return cached.conn
-    if(!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI,{
-    bufferCommands:false
-
-})
+ 
+    if (!MONGODB_URI) {
+        throw new Error("Mongo DB URL is not defined. Please add it to your environment variables.")
     }
 
-cached.con = await cached.promise
-     return cached.conn
-    
+    if (cached.conn) return cached.conn
+
+    if(!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        }
+
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+            return mongoose
+        })
+    }
+
+    try {
+        cached.conn = await cached.promise
+    } catch (e) {
+        cached.promise = null
+        throw e
+    }
+
+    return cached.conn
 }
